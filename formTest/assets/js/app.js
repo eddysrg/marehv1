83b6034fc25112bd6967b273dd7d28eed8c16217
Eduardo Ramírez <eddysrg@outlook.com>
2024-01-31 15:32:12 -0600

    ultimos cambios

--- formTest/assets/js/app.js
+++ formTest/assets/js/app.js
@@ -1,65 +1,131 @@
 //Codigo para que los botones avancen y retrocedan las secciones
-const buttons = document.querySelectorAll(".form__button");
-
-/* buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    if (button.classList.contains("button--previous")) {
-      handlePreviousClick(e.target);
-    } else if (button.classList.contains("btn-submit")) {
-      submitButton();
-    } else {
-      handleNextClick(e.target);
-    }
-  });
-}); */
 
 function submitButton() {
   console.log("Enviando informacion xd");
 }
 
 function handleNextClick(section) {
-  const currentElement = section;
-  const nextElement = currentElement.nextElementSibling;
+  const nextElement = section.nextElementSibling;
 
   if (nextElement) {
-    currentElement.style.display = "none";
+    section.style.display = "none";
     nextElement.style.display = "block";
   }
 }
 
-function handlePreviousClick(element) {
-  element.preventDefault();
-  const currentElement = element.parentElement.parentElement;
-  const previousElement = currentElement.previousElementSibling;
+function handlePreviousClick(section) {
+  const previousElement = section.previousElementSibling;
 
   if (previousElement) {
-    currentElement.style.display = "none";
+    section.style.display = "none";
     previousElement.style.display = "block";
   }
 }
-
-const formularios = document.querySelectorAll(".form__formulario");
-formularios[1].addEventListener("blur", (e) => console.log(e));
 
 //Codigo para validar los inputs
 
 const radioInputs = document.querySelectorAll('.form__input[type="radio"]');
 radioInputs.forEach((radioInput) =>
-  radioInput.addEventListener("change", (event) => validarInput(event))
+  radioInput.addEventListener("change", validarInput)
 );
+
+const formularios = document.querySelectorAll(".form__formulario");
+const formFirstSection = formularios[0];
+const formSecondSection = formularios[1];
+const formThirdSection = formularios[2];
+
+const valorInmueble = formSecondSection.querySelector("#v-inm");
+const valorIngreso = formSecondSection.querySelector("#ingreso");
+const valorCredito = formSecondSection.querySelector("#credito");
+
+const nombreCompleto = formThirdSection.querySelector("#full-name");
+const telefono = formThirdSection.querySelector("#phone-number");
+const email = formThirdSection.querySelector("#email");
+
+valorInmueble.addEventListener("blur", validarInput);
+valorIngreso.addEventListener("blur", validarInput);
+valorCredito.addEventListener("blur", validarInput);
+
+nombreCompleto.addEventListener("blur", validarInput);
+telefono.addEventListener("blur", validarInput);
+email.addEventListener("blur", validarInput);
+
+const buttons = document.querySelectorAll(".button");
+console.log(buttons);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    validarButton(event, button);
+  });
+});
+
+function validarButton(e, boton) {
+  e.preventDefault();
+
+  const nextButton = boton.classList.contains("btn-next");
+  const previousButton = boton.classList.contains("button--previous");
+  const actualSection = boton.parentElement.parentElement.parentElement;
+
+  if (nextButton) {
+    handleNextClick(actualSection);
+  } else if (previousButton) {
+    handlePreviousClick(actualSection);
+  } else {
+    console.log("Es submit");
+  }
+}
+
+function validarForm(formulario, seccionActual) {
+  const inputs = [...formulario.querySelectorAll(".form__input")];
+  const noInputsEmpty = inputs.every((input) => input.value !== "");
+
+  const formButton = formulario.querySelector(".button");
+
+  if (!formButton.classList.contains(".button--previous") && noInputsEmpty) {
+    console.log(formButton);
+  }
+}
 
 function validarInput(event) {
   const currentForm = event.target.parentElement.parentElement;
+  const currentSection = currentForm.parentElement.parentElement;
 
   if (currentForm.id === "form-radio") {
     currentForm.querySelector(".btn-next").disabled = false;
     currentForm.addEventListener("click", (e) => {
       e.preventDefault();
-      handleNextClick(currentForm.parentElement.parentElement);
+      handleNextClick(currentSection);
     });
+
+    return;
   }
+
+  if (event.target.value === "") {
+    imprimirAviso("Favor de llenar el campo", event.target.parentElement);
+  } else {
+    eliminarAviso(event.target.parentElement);
+    validarForm(currentForm, currentSection);
+  }
+}
+
+function imprimirAviso(mensaje, elemento) {
+  if (!elemento.lastElementChild.classList.contains("aviso")) {
+    const parrafo = document.createElement("p");
+    parrafo.textContent = mensaje;
+    parrafo.classList.add("aviso");
+
+    elemento.appendChild(parrafo);
+
+    elemento.querySelector(".form__input").classList.add("empty-input");
+  }
+}
+
+function eliminarAviso(elemento) {
+  if (elemento.lastElementChild.classList.contains("aviso")) {
+    elemento.lastElementChild.remove();
+  }
+
+  elemento.querySelector(".form__input").classList.remove("empty-input");
 }
 
 //Codigo para hacer el calculo de los valores llenados en el form
